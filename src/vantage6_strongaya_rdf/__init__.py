67580efdb973f0a1606eb7a8eca3518ba6c8bba9
JHogenboom <106407313+JHogenboom@users.noreply.github.com>
2026-08-11 16:05:27 +0200

    feat: export get_schema_prefixes from schema_parser

--- src/vantage6_strongaya_rdf/__init__.py
+++ src/vantage6_strongaya_rdf/__init__.py
@@ -6,6 +6,7 @@ from .collect_sparql_data import collect_sparql_data
 from .schema_loader import load_schema
 from .schema_parser import (
     build_predicate_path,
+    get_schema_prefixes,
     get_variable_query_params,
     resolve_intermediate_class_path,
 )
@@ -14,6 +15,7 @@ __all__ = [
     "collect_sparql_data",
     "load_schema",
     "build_predicate_path",
+    "get_schema_prefixes",
     "get_variable_query_params",
     "resolve_intermediate_class_path",
 ]
